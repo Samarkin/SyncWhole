b73aa91724031865378d73df7510a64acb5fd75a
Pavel Samarkin <samarkinpa@gmail.com>
2019-01-24 15:33:31 -0800

    Prototype Outlook COM API adapter

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SyncWhole
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._btnLoad = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this._lblDetails = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// _btnLoad
+			// 
+			this._btnLoad.Location = new System.Drawing.Point(12, 12);
+			this._btnLoad.Name = "_btnLoad";
+			this._btnLoad.Size = new System.Drawing.Size(75, 23);
+			this._btnLoad.TabIndex = 0;
+			this._btnLoad.Text = "Load";
+			this._btnLoad.UseVisualStyleBackColor = true;
+			this._btnLoad.Click += new System.EventHandler(this.ButtonClick);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(12, 41);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(352, 381);
+			this.listBox1.TabIndex = 1;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBoxSelectionChanged);
+			// 
+			// _lblDetails
+			// 
+			this._lblDetails.AutoSize = true;
+			this._lblDetails.Location = new System.Drawing.Point(370, 41);
+			this._lblDetails.Name = "_lblDetails";
+			this._lblDetails.Size = new System.Drawing.Size(16, 13);
+			this._lblDetails.TabIndex = 2;
+			this._lblDetails.Text = "...";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this._lblDetails);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this._btnLoad);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1Closing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label _lblDetails;
+		private System.Windows.Forms.Button _btnLoad;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace SyncWhole
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private async void ButtonClick(object sender, EventArgs e)
+		{
+			_btnLoad.Enabled = false;
+			try
+			{
+				listBox1.Items.Clear();
+				await Task.Run(() => LoadAppointments(vm => BeginInvoke((Action)(() => listBox1.Items.Add(vm)))));
+			}
+			finally
+			{
+				_btnLoad.Enabled = true;
+			}
+		}
+
+		private static void LoadAppointments(Action<AppointmentViewModel> callback)
+		{
+			var app = new Outlook.Application();
+			var calendar = app.Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar);
+
+			foreach (Outlook.AppointmentItem item in calendar.Items)
+			{
+				callback(new AppointmentViewModel(item));
+			}
+		}
+
+
+		private class AppointmentViewModel
+		{
+			private readonly Outlook.AppointmentItem _appointment;
+
+			public AppointmentViewModel(Outlook.AppointmentItem appointment)
+			{
+				_appointment = appointment;
+			}
+
+			public override string ToString()
+			{
+				return _appointment.Subject ?? "<null>";
+			}
+
+			private static string ToString(Outlook.OlDaysOfWeek daysOfWeek)
+			{
+				KeyValuePair<Outlook.OlDaysOfWeek, string>[] arr =
+				{
+					new KeyValuePair<Outlook.OlDaysOfWeek, string> (Outlook.OlDaysOfWeek.olMonday, "Monday"),
+					new KeyValuePair<Outlook.OlDaysOfWeek, string> (Outlook.OlDaysOfWeek.olTuesday, "Tuesday"),
+					new KeyValuePair<Outlook.OlDaysOfWeek, string> (Outlook.OlDaysOfWeek.olWednesday, "Wednesday"),
+					new KeyValuePair<Outlook.OlDaysOfWeek, string> (Outlook.OlDaysOfWeek.olThursday, "Thursday"),
+					new KeyValuePair<Outlook.OlDaysOfWeek, string> (Outlook.OlDaysOfWeek.olFriday, "Friday"),
+					new KeyValuePair<Outlook.OlDaysOfWeek, string> (Outlook.OlDaysOfWeek.olSaturday, "Saturday"),
+					new KeyValuePair<Outlook.OlDaysOfWeek, string> (Outlook.OlDaysOfWeek.olSunday, "Sunday"),
+				};
+
+				return string.Join(", ", arr.Where(kv => daysOfWeek.HasFlag(kv.Key)).Select(kv => kv.Value));
+			}
+
+			public string ToLongString()
+			{
+				StringBuilder sb = new StringBuilder();
+				sb.AppendLine(_appointment.BusyStatus.ToString());
+				sb.AppendLine(_appointment.Subject);
+				sb.AppendLine(_appointment.Location);
+				if (_appointment.AllDayEvent)
+				{
+					sb.AppendLine($"{_appointment.Start.ToShortDateString()}");
+				}
+				else if (_appointment.Start.Date == _appointment.End.Date)
+				{
+					sb.AppendLine($"{_appointment.Start} - {_appointment.End.ToShortTimeString()}");
+				}
+				else
+				{
+					sb.AppendLine($"{_appointment.Start} - {_appointment.End}");
+				}
+
+				if (_appointment.IsRecurring)
+				{
+					var pattern = _appointment.GetRecurrencePattern();
+					switch (pattern.RecurrenceType)
+					{
+						case Outlook.OlRecurrenceType.olRecursDaily:
+							sb.Append($"Repeat daily every {pattern.Interval} days");
+							break;
+						case Outlook.OlRecurrenceType.olRecursWeekly:
+							sb.Append($"Repeat weekly every {pattern.Interval} week on {ToString(pattern.DayOfWeekMask)}");
+							break;
+						case Outlook.OlRecurrenceType.olRecursMonthly:
+							sb.Append($"Repeat monthly every {pattern.Interval} month on the {pattern.DayOfMonth} day of the month");
+							break;
+						case Outlook.OlRecurrenceType.olRecursYearly:
+							sb.Append($"Repeat yearly on the {pattern.DayOfMonth} day of the {pattern.MonthOfYear} month");
+							break;
+						default:
+							throw new ArgumentOutOfRangeException();
+					}
+					sb.AppendLine($" until {(pattern.NoEndDate ? "pigs fly" : pattern.PatternEndDate.ToShortDateString())}");
+
+					foreach (Outlook.Exception exception in pattern.Exceptions)
+					{
+						if (exception.Deleted)
+						{
+							sb.AppendLine($"  except on {exception.OriginalDate.ToShortDateString()}");
+						}
+						else if (exception.AppointmentItem.Start.ToShortTimeString() != _appointment.Start.ToShortTimeString()
+							|| exception.AppointmentItem.End.ToShortTimeString() != _appointment.End.ToShortTimeString())
+						{
+							sb.AppendLine($"  but on {exception.OriginalDate.ToShortDateString()} @ {exception.AppointmentItem.Start.ToShortTimeString()} - {exception.AppointmentItem.End.ToShortTimeString()}");
+						}
+						else if (exception.AppointmentItem.Subject != _appointment.Subject)
+						{
+							sb.AppendLine($"  but on {exception.OriginalDate.ToShortDateString()} - {exception.AppointmentItem.Subject}");
+						}
+						else if (exception.AppointmentItem.Location != _appointment.Location)
+						{
+							sb.AppendLine($"  but on {exception.OriginalDate.ToShortDateString()} @ {exception.AppointmentItem.Location}");
+						}
+						else
+						{
+							sb.AppendLine($"  but on {exception.OriginalDate.ToShortDateString()} it is different somehow");
+						}
+					}
+				}
+
+				sb.AppendLine();
+				sb.AppendLine(_appointment.GlobalAppointmentID);
+
+				return sb.ToString();
+			}
+		}
+
+		private void ListBoxSelectionChanged(object sender, EventArgs e)
+		{
+			var apt = listBox1.SelectedItem as AppointmentViewModel;
+			if (apt == null)
+			{
+				return;
+			}
+
+			_lblDetails.Text = apt.ToLongString();
+		}
+
+		private void Form1Closing(object sender, FormClosingEventArgs e)
+		{
+		}
+	}
+}
